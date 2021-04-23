d08bc96b89d0a6f9c702bbb00ac7d63e079306d3
oskarcokl <oskar.cokl@gmail.com>
2021-04-23 11:55:29 +0200

    Added container.js cmponent.

--- components/button.js
+++ components/button.js
@@ -1,3 +1,7 @@
 export default function Button({ name }) {
-  return <button class="bg-purple-600 w-44">{name}</button>;
+  return (
+    <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+      {name}
+    </button>
+  );
 }
