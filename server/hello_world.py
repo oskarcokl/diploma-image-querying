cfe02f1eaf62d3842b8810fdfbd2fa9aedaef0a2
oskarcokl <oskar.cokl@gmail.com>
2021-04-28 22:48:03 +0200

    Added a parser for command line arguments when starting the server.

--- server/hello_world.py
+++ server/hello_world.py
@@ -1,16 +1,28 @@
 import tornado.ioloop
 import tornado.web
 
+from tornado.options import define, options, parse_command_line
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=True, help="run in debug mode")
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
-def make_app():
-    return tornado.web.Application([
+class HelloHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello You made it to a different route")  
+
+
+def main():
+    parse_command_line()
+    app = tornado.web.Application([
         (r"/", MainHandler),
+        (r"/hello", HelloHandler),
     ])
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    main()
