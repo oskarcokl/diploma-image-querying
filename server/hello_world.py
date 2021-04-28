904adc2f8c10de7bb084d38cb87df6c45f75984b
oskarcokl <oskar.cokl@gmail.com>
2021-04-28 23:06:44 +0200

    Made changes to the readme file.

--- server/hello_world.py
+++ server/hello_world.py
@@ -6,23 +6,29 @@ from tornado.options import define, options, parse_command_line
 define("port", default=8888, help="run on the given port", type=int)
 define("debug", default=True, help="run in debug mode")
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+
 class HelloHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello You made it to a different route")  
+        self.write("Hello You made it to a different route")
 
 
 def main():
     parse_command_line()
-    app = tornado.web.Application([
-        (r"/", MainHandler),
-        (r"/hello", HelloHandler),
-    ])
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/hello", HelloHandler),
+        ],
+        debug=options.debug,
+    )
     app.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
+
 
 if __name__ == "__main__":
     main()
