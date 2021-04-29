5b20ab5d1d2ffd4b693a7aba465c525b78a63885
oskarcokl <oskar.cokl@gmail.com>
2021-04-29 12:02:51 +0200

    Tested doing async fetch and converting the returned value to json.

--- server/hello_world.py
+++ server/hello_world.py
@@ -1,5 +1,7 @@
 import tornado.ioloop
 import tornado.web
+import json
+from tornado.httpclient import AsyncHTTPClient
 
 from tornado.options import define, options, parse_command_line
 
@@ -17,12 +19,26 @@ class HelloHandler(tornado.web.RequestHandler):
         self.write("Hello You made it to a different route")
 
 
+class JsonHandler(tornado.web.RequestHandler):
+    async def get(self):
+        response = await self.async_fetch("http://localhost:3000/images")
+        print(type(json.loads(response)[0]))
+        self.write("Just testing some things :^)")
+
+    async def async_fetch(self, url):
+        http_client = AsyncHTTPClient()
+        response = await http_client.fetch(url)
+        string_value = response.body.decode("utf-8")
+        return string_value
+
+
 def main():
     parse_command_line()
     app = tornado.web.Application(
         [
             (r"/", MainHandler),
             (r"/hello", HelloHandler),
+            (r"/json", JsonHandler),
         ],
         debug=options.debug,
     )
