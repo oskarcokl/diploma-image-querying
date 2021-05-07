f674da9d521968d00021ea9f36c60b1804b74d95
oskarcokl <oskar.cokl@gmail.com>
2021-05-07 10:42:31 +0200

    feat: Fixed aligning in component-builder.js.

--- pages/component-building.js
+++ pages/component-building.js
@@ -2,7 +2,7 @@ import FileUpload from "../components/fileUpload";
 
 export default function componentBuilding(params) {
   return (
-    <div className="text-center my-52">
+    <div className="grid place-items-center mt-52">
       <FileUpload></FileUpload>
     </div>
   );
