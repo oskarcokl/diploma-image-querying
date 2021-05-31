6265272813d5e8b6ef1e60560dd2c99e6a88bead
oskarcokl <oskar.cokl@gmail.com>
2021-05-31 11:21:57 +0200

    feat: Can now connect to database running in docker container.

--- server/scripts/cnn/index.py
+++ server/scripts/cnn/index.py
@@ -1,3 +1,32 @@
 import psycopg2
+from config import config
 
-connection = psycopg2.connect(filename="database.ini", section="postgresql")
+
+def connect():
+    # Connect to PostgreSQL database
+    connection = None
+
+    try:
+        params = config()
+        print("Connectino to PostgresQL database...")
+        connection = psycopg2.connect(**params)
+
+        cursor = connection.cursor()
+
+        print("PostgreSQL databse version: ")
+        cursor.execute("SELECT version()")
+
+        db_version = cursor.fetchone()
+        print(db_version)
+
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as e:
+        print(e)
+    finally:
+        if connection is not None:
+            connection.close()
+            print("Database connection close.")
+
+
+if __name__ == "__main__":
+    connect()
