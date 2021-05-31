d4970aee9bc9c2289cbff326f347baa72e183871
oskarcokl <oskar.cokl@gmail.com>
2021-05-31 15:17:37 +0200

    feat: Add function to add image_vector to db.

--- server/scripts/cnn/index.py
+++ server/scripts/cnn/index.py
@@ -28,5 +28,30 @@ def connect():
             print("Database connection close.")
 
 
+def insert_image_vector(image_name, image_vector):
+    sql = """INSERT INTO cbir_index(image_name, image_vector) 
+             VALUES(%s, %s) RETURNING id;"""
+
+    connection = None
+    image_id = None
+    try:
+        params = config()
+        connection = psycopg2.connect(**params)
+        cursor = connection.cursor()
+        cursor.execute(sql, (image_name, image_vector))
+        image_id = cursor.fetchone()[0]
+        connection.commit()
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as e:
+        print(e)
+    finally:
+        if connection is not None:
+            connection.close()
+
+    return image_id
+
+
 if __name__ == "__main__":
-    connect()
+    image_name = "example1.jpg"
+    image_vector = [69] * 4096
+    print(insert_image_vector(image_name, image_vector))
