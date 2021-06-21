0305a7ccb488c5b441b0d3eaeb4967ec87c437a2
oskarcokl <oskar.cokl@gmail.com>
2021-06-21 16:33:26 +0200

    feat: Add _Leaf internal class to CDTree class.
    The class will represent the Leaf of the tree.

--- server/models/CDTree.py
+++ server/models/CDTree.py
@@ -3,25 +3,44 @@ class CDTree:
         pass
 
 
-"""
-Inner node of CDTree:
-
-Parameters
-==========
-n_sub_clutsers: int
-    Number of sub clusters of this node
-
-gmm_parameters: list
-    Parameters of the gmm model asosiated with this cluster
-
-sub_nodes: list
-    List of pointers to sub nodes (clusters)
-
-"""
 class _Node:
+    """
+    Inner node of CDTree:
+
+    Parameters
+    ==========
+
+    n_sub_clutsers: int
+        Number of sub clusters of this node
+
+    gmm_parameters: list
+        Parameters of the gmm model asosiated with this cluster
+
+    sub_nodes: list
+        List of pointers to sub nodes (clusters)
+
+    """
+
     def __init__(self, n_sub_clutsers, gmm_parameters, sub_nodes):
         self.n_sub_clutsers = n_sub_clutsers
         self.gmm_parameters = gmm_parameters
         self.sub_nodes = sub_nodes
 
+
 class _Leaf:
+    """
+    Leaf node of CDTree:
+
+    Parameters
+    ==========
+
+    n_feature_vectors: int
+        Number of feature vectors in this leaf
+
+    ids: list
+        List of ids in this leaf.
+    """
+
+    def __init__(self, n_feature_vectors, ids):
+        self.n_feature_vectors = n_feature_vectors
+        self.ids = ids
