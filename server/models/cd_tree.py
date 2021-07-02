15a94d466f325197a8a19a7cfe42dc0acde32415
oskarcokl <oskar.cokl@gmail.com>
2021-07-02 15:15:21 +0200

    refactor: Moved _Node to own file.

--- server/models/cd_tree.py
+++ server/models/cd_tree.py
@@ -5,6 +5,7 @@ from sklearn import mixture
 from scipy import stats
 
 from .gmm import GMM
+from node import Node
 
 # TODO refactor to not be a class and rather a module.
 
@@ -152,7 +153,7 @@ class CDTree(persistent.Persistent):
             ids.append(item[0])
             feature_vectors.append(item[2])
 
-        root_node = _Node(
+        root_node = Node(
             n_feature_vectors=len(data),
             is_leaf=False,
             ids=ids,
@@ -305,7 +306,7 @@ class CDTree(persistent.Persistent):
         # a new node for each one.
         for index in range(n_clusters):
             self.node_id += 1
-            sub_node = _Node(layer=sub_node_layer, node_id=self.node_id)
+            sub_node = Node(layer=sub_node_layer, node_id=self.node_id)
 
             feature_vectors = []
             ids = []
@@ -385,155 +386,6 @@ class CDTree(persistent.Persistent):
             sum += resp[1]
 
         return sum
-
-
-class _Node(persistent.Persistent):
-    # TODO seperate into own file.
-    """
-    Inner node of CDTree:
-
-    Parameters
-    ----------
-
-    n_sub_clusters: int
-        Number of sub clusters of this node
-
-    gmm_parameters: dict
-        Parameters of the gmm model asosiated with this cluster
-
-        Parameters
-        ----------
-        means: list
-            Mean of each gmm cluster.
-
-        covs_array: nd_array
-            Covariance matrix of each cluster.
-
-        weights: list
-            Weight that each cluster contributes to the gmm.
-
-    sub_nodes: list
-        List of pointers to sub nodes (clusters)
-
-    n_feature_vectors: int
-        Number of feature vectors in this leaf
-
-    ids: list
-        List of ids in this leaf.
-
-    is_leaf: bool
-        Boolean which determines if the node is a leaf or not.
-
-    is_root: bool
-        Wheather the node is the rote node of the tree or not.
-
-    layer: int
-        Which layer is the node on.
-
-    data: []
-        Same as data that gets based into CDTree.init_cd_tree()
-        but only keeping the relevant parts for the node.
-
-    """
-
-    def __init__(
-        self,
-        is_leaf=False,
-        n_feature_vectors=-1,
-        ids=[],
-        n_sub_clusters=-1,
-        gmm_parameters={},
-        sub_nodes=[],
-        is_root=False,
-        layer=-1,
-        data=[],
-        feature_vectors=[],
-        node_id=-1,
-    ):
-        self.is_leaf = is_leaf
-        self.n_feature_vectors = n_feature_vectors
-        self.ids = ids
-        self.is_root = is_root
-        self.n_sub_clusters = n_sub_clusters
-        self.gmm_parameters = gmm_parameters
-        self.sub_nodes = sub_nodes
-        self.layer = layer
-        self.data = data
-        self.feature_vectors = feature_vectors
-        self.node_id = node_id
-
-    def __str__(self):
-        return """
-    Information about nodes paramteres
-    ==================================
-    is_leaf: {is_leaf}
-    n_feature_vectors: {n_feature_vectors}
-    ids: {ids}
-    n_sub_clutsers: {n_sub_clusters}
-    gmm_parameters: {gmm_parameters}
-    sub_nodes: {sub_nodes}
-    is_root: {is_root}
-    layer: {layer}
-    node_id: {node_id}
-    ==================================
-        """.format(
-            is_leaf=self.is_leaf,
-            n_feature_vectors=self.n_feature_vectors,
-            ids=self.ids,
-            is_root=self.is_root,
-            n_sub_clusters=self.n_sub_clusters,
-            gmm_parameters=self.gmm_parameters,
-            sub_nodes=self.sub_nodes,
-            layer=self.layer,
-            node_id=self.node_id,
-        )
-
-    # This function assumes that ids are ordered.
-    # Return a list[id:int, feature_vector:[]]
-    def get_node_features(self):
-        feature_vectors_with_ids = []
-
-        for item in self.data:
-            vector_with_id = [item[0], item[1]]
-            feature_vectors_with_ids.append(vector_with_id)
-
-        return feature_vectors_with_ids
-
-    def set_feature_vectors(self, feature_vectors):
-        self.feature_vectors = feature_vectors
-        self._p_changed = True
-
-    def set_ids(self, ids):
-        self.ids = ids
-        self._p_changed = True
-
-    def set_sub_nodes(self, sub_nodes):
-        self.sub_nodes = sub_nodes
-        self._p_changed = True
-
-    def set_gmm_parameters(self, gmm_parameters):
-        self.gmm_parameters = gmm_parameters
-        self._p_changed = True
-
-    def set_covs_array(self, covs_array):
-        self.gmm_parameters["covs_array"] = covs_array
-        self._p_changed = True
-
-    def set_means(self, means):
-        self.gmm_parameters["means"] = means
-        self._p_changed = True
-
-    def set_is_leaft(self, is_leaf):
-        self.is_leaf = is_leaf
-        self._p_changed = True
-
-    def add_id(self, id):
-        self.ids.append(id)
-        self._p_changed = True
-
-    def add_feature_vector(self, feature_vector):
-        self.feature_vectors.append(feature_vector)
-        self._p_changed = True
 
 
 # Used for quick testing during developement.
