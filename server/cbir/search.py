7fb6c70d7a552b6979199bd1671e369f9dbd0ae5
oskarcokl <oskar.cokl@gmail.com>
2021-07-03 19:52:22 +0200

    feat: Using SVD to lower dimensions of query vector.
    This is kinda bad beacuse we need to access the database and do an SVD.
    Should work better with a autoencoder.

--- server/cbir/search.py
+++ server/cbir/search.py
@@ -9,6 +9,12 @@ from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras import backend as K
+from sklearn.decomposition import TruncatedSVD
+
+import sys
+sys.path.insert(0, "../")
+
+from db_connector import DbConnector
 
 
 def search(query_img_path=None, query_img_list=None, cli=False, dataset=""):
@@ -34,8 +40,6 @@ def search(query_img_path=None, query_img_list=None, cli=False, dataset=""):
             img_names = find_similar_imgs(
                 img_array=img_array, model=model, searcher=searcher
             )
-
-            print(img_names)
 
             img_paths = [os.path.join(dataset, img_name)
                          for img_name in img_names]
@@ -72,8 +76,46 @@ def find_similar_imgs(img_array, model, searcher):
         [model.layers[0].input], model.layers[22].output)
     features_query = get_fc2_layer_output([processed_img_array])[0]
 
-    img_names = searcher.search(features_query.reshape(1, -1), 10)
-    return img_names
+    feature_query_2d = np.array([features_query])
+
+    print(feature_query_2d.shape)
+
+    reduced_feature_query = reduce_features(feature_query_2d)
+    print(reduced_feature_query)
+
+    #img_names = searcher.search(features_query.reshape(1, -1), 10)
+    # return img_names
+
+
+def reduce_features(query_features, n_components=100):
+    feature_vectors = get_data()
+    feature_vectors_plus = np.append(
+        feature_vectors, np.array(query_features), axis=0)
+
+    feature_array = np.array(feature_vectors_plus)
+    svd = TruncatedSVD(n_components=n_components)
+    svd.fit(feature_array)
+    result = svd.transform(feature_array)
+    query_reduced = result[len(result) - 1]
+    return query_reduced
+
+
+def get_data():
+    connector = DbConnector()
+    connector.cursor.execute("SELECT * FROM cbir_index")
+    print("Number of indexed images: ", connector.cursor.rowcount)
+    data = connector.cursor.fetchall()
+    data_array = np.array(data, dtype=object)
+
+    # rand_indexes = np.random.choice(
+    #     1909, 1909, replace=False
+    # )
+    # print(rand_indexes)
+    # rand_data = data_array[rand_indexes]
+    # print(f"Lenght of subset of data {len(rand_data)}")
+    feature_vectors = data_array[:, 2]
+    result = [np.array(feature_vector) for feature_vector in feature_vectors]
+    return np.array(result)
 
 
 if __name__ == "__main__":
