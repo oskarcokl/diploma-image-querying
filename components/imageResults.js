5eb24ed7b65fabd13dc821ec54a6819758205462
oskarcokl <oskar.cokl@gmail.com>
2021-07-06 10:19:33 +0200

    Refactor: Changed result images to collumn.

--- components/imageResults.js
+++ components/imageResults.js
@@ -8,16 +8,14 @@ export default function ImageResults({ srcs, styleName }) {
       <div className="result-images-container">
         <div className="column-1">
           <ResultImage src={srcs[0]}></ResultImage>
-          <ResultImage src={srcs[2]}></ResultImage>
-          <ResultImage src={srcs[4]}></ResultImage>
-          <ResultImage src={srcs[6]}></ResultImage>
-          <ResultImage src={srcs[8]}></ResultImage>
-        </div>
-        <div className="column-2">
           <ResultImage src={srcs[1]}></ResultImage>
+          <ResultImage src={srcs[2]}></ResultImage>
           <ResultImage src={srcs[3]}></ResultImage>
+          <ResultImage src={srcs[4]}></ResultImage>
           <ResultImage src={srcs[5]}></ResultImage>
+          <ResultImage src={srcs[6]}></ResultImage>
           <ResultImage src={srcs[7]}></ResultImage>
+          <ResultImage src={srcs[8]}></ResultImage>
           <ResultImage src={srcs[9]}></ResultImage>
         </div>
       </div>
