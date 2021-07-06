8696550db6fe3bc8594bb326665896096ee584ba
oskarcokl <oskar.cokl@gmail.com>
2021-07-06 16:41:54 +0200

    feat: Add now accepts list of images from cli.

--- server/cbir/add.py
+++ server/cbir/add.py
@@ -7,4 +7,8 @@ sys.path.insert(0, "./")
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
-    argParser.add_argument
+    argParser.add_argument("-i", "--images", nargs="+", required=True,
+                           help="Pass paths to images you want to add to index.")
+
+    args = vars(argParser.parse_args())
+    print(args)
