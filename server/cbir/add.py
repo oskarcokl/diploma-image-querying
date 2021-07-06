c4d87d2c0da032cf6e981826bee3451b77929a1d
oskarcokl <oskar.cokl@gmail.com>
2021-07-06 16:17:21 +0200

    refactor: Moved add.py to apropriate directory.

---
+++ server/cbir/add.py
@@ -0,0 +1,10 @@
+import argparse
+import os
+
+import sys
+sys.path.insert(0, "../")
+sys.path.insert(0, "./")
+
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument
