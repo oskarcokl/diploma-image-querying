65460a348729c462c911944a85fc32d1146d0937
oskarcokl <oskar.cokl@gmail.com>
2021-07-06 10:33:31 +0200

    refactor: Add propers links to navbar.

--- components/navbar.js
+++ components/navbar.js
@@ -1,10 +1,10 @@
 export default function Navbar({ styleName }) {
   return (
     <div className={`navbar ${styleName}`}>
-      <a href="#" className={`navbarElement`}>
+      <a href="query-by-example" className={`navbarElement`}>
         query by example
       </a>
-      <a href="#" className={`navbarElement`}>
+      <a href="add-to-index" className={`navbarElement`}>
         add images to index
       </a>
     </div>
