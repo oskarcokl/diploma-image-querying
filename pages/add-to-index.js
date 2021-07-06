6e8d88849b3200db4a16c88b66dd42f2a1c2aabc
oskarcokl <oskar.cokl@gmail.com>
2021-07-06 10:29:06 +0200

    feat: Add page add-to-index.
    Page will be used as frontend for adding images into the database and cd
    tree.

---
+++ pages/add-to-index.js
@@ -0,0 +1,41 @@
+import Head from "next/dist/next-server/lib/head";
+import Container from "../components/container";
+import Navbar from "../components/navbar";
+import PageTitle from "../components/pageTitle";
+import SectionTitle from "../components/sectionTitle";
+import FileUpload from "../components/fileUpload";
+import UploadedImage from "../components/uploadedImage";
+import Button from "../components/button";
+
+export default function AddToIndex(params) {
+  return (
+    <div>
+      <Head>
+        <title>QueryByExample</title>
+      </Head>
+      <Container>
+        <div className="query-exmaple-grid-container">
+          <Navbar styleName="query-example-grid-navbar"></Navbar>
+          <PageTitle
+            title="Add images to index"
+            styleName="query-example-grid-header"
+          />
+          <form className="image-upload-container query-example-grid-query">
+            <SectionTitle title="Upload example image" />
+            <UploadedImage /*src={imageSrc}*/ />
+            <FileUpload
+              additionalClasses=""
+              name={"file"}
+              //onChangeHandler={onChangeHandler}
+            ></FileUpload>
+            <Button
+              additionalClasses="flex-none"
+              name="Upload image"
+              //clickHandler={onClickHandler}
+            ></Button>
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
+}
