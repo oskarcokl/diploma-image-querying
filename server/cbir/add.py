592d91b785830e0f5afbe1717e7a09208d0fba36
oskarcokl <oskar.cokl@gmail.com>
2021-07-07 12:22:56 +0200

    feat: Add functions get_data and reduce features.

--- server/cbir/add.py
+++ server/cbir/add.py
@@ -8,14 +8,14 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.preprocessing import image
 from tensorflow import keras
 from sklearn import preprocessing
+from sklearn.decomposition import TruncatedSVD
 import numpy as np
 
 import sys
 sys.path.insert(0, "../")
 sys.path.insert(0, "./")
-
-
 from adder import Adder
+from db_utils.db_connector import DbConnector
 
 
 def add_cli(img_list):
@@ -56,6 +56,29 @@ def add_cli(img_list):
     for i in range(len(img_name_list)):
         id = adder.add_img_to_db(normalized_features[i], img_name_list[i])
         ids.append(id)
+
+
+def reduce_features(query_features, n_components=100):
+    feature_vectors = get_data()
+    feature_vectors_plus = np.append(
+        feature_vectors, np.array(query_features), axis=0)
+
+    feature_array = np.array(feature_vectors_plus)
+    svd = TruncatedSVD(n_components=n_components)
+    svd.fit(feature_array)
+    result = svd.transform(feature_array)
+    query_reduced = result[len(result) - 1]
+    return query_reduced
+
+
+def get_data():
+    connector = DbConnector()
+    connector.cursor.execute("SELECT * FROM cbir_index")
+    print("Number of indexed images: ", connector.cursor.rowcount)
+    data = connector.cursor.fetchall()
+    data_array = np.array(data, dtype=object)
+
+    return data_array
 
 
 def normalize_sk_learn(feature_list):
