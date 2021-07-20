6505a7cfe1a924a37123c01965742cf81778c105
oskarcokl <oskar.cokl@gmail.com>
2021-07-20 11:06:34 +0200

    refactor: load_model() will now get model with specific output layer.
    The current layer is the second to last, which is the FC layer with 4096
    dims.

--- server/cbir/backbone.py
+++ server/cbir/backbone.py
@@ -1,19 +1,22 @@
 import os
 
 from tensorflow import keras
-from tensorflow.keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
 
 
 class Backbone:
     model = None
 
-    def get_model(self):
+    def load_model(self):
         if os.path.isdir("./vgg16"):
-            self.model = self._load_model_from_disk()
+            model = self._load_model_from_disk()
         else:
-            self.model = self._download_model()
-            self._save_model_to_disk(self, self.model)
-        return self.model
+            model = self._download_model()
+            self._save_model_to_disk(model)
+
+        self.model = Model(inputs=model.inputs,
+                           outputs=model.layers[-2].output)
 
     def _load_model_from_disk(self):
         print("Model already downloaded loading from disk.")
