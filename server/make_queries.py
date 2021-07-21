157dcd38606fd5f057c0ab3367c69c4092d8d6aa
oskarcokl <oskar.cokl@gmail.com>
2021-07-21 13:29:26 +0200

    feat: Add make_queries.py
    The module will be used to make the queries which will later be used for
    evaluation.

---
+++ server/make_queries.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import sys
+
+
+from cbir import search
+sys.path.insert(0, "./cbir/")
+
+def make_queries(file_name):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        print(lines)
+        for line in lines:
+            query_img_name = line[:-1]
+            query_img_path = os.path.join("../vacations", query_img_name)
+
+            print(query_img_path)
+            # search(query_img_name, cli=True, dataset="")
+
+
+if __name__ == "__main__":
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument(
+        "-f",
+        "--file",
+        help="Path to file containing query names in columns."
+    )
+    args = vars(argParser.parse_args())
+
+    make_queries(args["file"])
