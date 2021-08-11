a24e7bca034086531cc8739d792caa22a8c5289c
oskarcokl <oskar.cokl@gmail.com>
2021-08-11 20:32:00 +0200

    feat: Images you want to add to index will now be shown before add.

---
+++ components/addImages.js
@@ -0,0 +1,24 @@
+import ResultImage from "./resultImage";
+import SectionTitle from "./sectionTitle";
+
+export default function ImageResults({ srcs, styleName }) {
+  if (srcs) {
+    console.log(srcs);
+
+    const addImages = [];
+    for (let i = 0; i < srcs.length; i++) {
+      addImages.push(<ResultImage src={srcs[i]} />);
+    }
+
+    return (
+      <div className={`image-results-container ${styleName}`}>
+        <SectionTitle title="Results" />
+        <div className="result-images-container">
+          <div className="column-1">{addImages}</div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+}
--- pages/add-to-index.js
+++ pages/add-to-index.js
@@ -2,12 +2,22 @@ import Head from "next/dist/next-server/lib/head";
 import Container from "../components/container";
 import Navbar from "../components/navbar";
 import PageTitle from "../components/pageTitle";
-import SectionTitle from "../components/sectionTitle";
 import FileUpload from "../components/fileUpload";
-import UploadedImage from "../components/uploadedImage";
+import AddImages from "../components/addImages";
 import Button from "../components/button";
+import { useState } from "react";
 
 export default function AddToIndex(params) {
+  const [addUrls, setAddUrls] = useState([]);
+
+  const onChangeHandler = async (e) => {
+    const urls = [];
+    for (let file of e.target.files) {
+      urls.push(URL.createObjectURL(file));
+    }
+    setAddUrls(urls);
+  };
+
   const onClickHandler = (e) => {
     e.preventDefault();
 
@@ -27,11 +37,12 @@ export default function AddToIndex(params) {
             styleName="query-example-grid-header"
           />
           <form className="image-upload-container query-example-grid-query">
-            <UploadedImage /*src={imageSrc}*/ />
+            <AddImages srcs={addUrls} styleName="query-example-grid-results" />
             <FileUpload
               additionalClasses=""
               name={"file"}
               text={"select images"}
+              onChangeHandler={onChangeHandler}
             ></FileUpload>
             <Button
               additionalClasses="flex-none"
