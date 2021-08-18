c1478a97f711a079c7302650263ec9baee07fdc9
oskarcokl <oskar.cokl@gmail.com>
2021-08-18 19:26:20 +0200

    feat: make_new_query now implements ROCCHIO algorithm.

--- server/rocchio.py
+++ server/rocchio.py
@@ -1,5 +1,17 @@
-def make_new_query(query_features, related_images, non_related_images, query_weight=1, related_documents_weight=0.8, non_related_documents_weight=0.1):
-    print(query_features)
-    print(len(related_images))
-    print(len(non_related_images))
-    return
+import numpy as np
+
+
+def make_new_query(query_features, related_images, non_related_images, a=1, b=0.8, c=0.1):
+
+    related_sum = np.zeros(query_features.size)
+    for related_features in related_images:
+        related_sum = related_sum + related_features
+
+    non_related_sum = np.zeros(query_features.size)
+    for non_related_features in non_related_images:
+        non_related_sum = non_related_sum + non_related_features
+
+    new_query = (a * query_features) + (b * 1 / len(related_images) *
+                                        related_sum) - (c * 1 / len(non_related_images) * non_related_sum)
+
+    return new_query
