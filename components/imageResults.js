c943d66d05f9926f3033e573bc52e50671210a07
oskarcokl <oskar.cokl@gmail.com>
2021-08-18 12:51:54 +0200

    feat: Add button to results images component.

--- components/imageResults.js
+++ components/imageResults.js
@@ -1,24 +1,24 @@
 import { MoonLoader } from "react-spinners";
 import ResultImage from "./resultImage";
 import SectionTitle from "./sectionTitle";
+import Button from "./button";
 
 export default function ImageResults({ srcs, styleName, onClick }) {
   return (
     <div className={`image-results-container ${styleName}`}>
       <SectionTitle title="Results" />
       <div className="result-images-container">
-        <div className="column-1">
-          <ResultImage src={srcs[0]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[1]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[2]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[3]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[4]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[5]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[6]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[7]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[8]} onClick={onClick}></ResultImage>
-          <ResultImage src={srcs[9]} onClick={onClick}></ResultImage>
-        </div>
+        <ResultImage src={srcs[0]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[1]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[2]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[3]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[4]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[5]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[6]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[7]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[8]} onClick={onClick}></ResultImage>
+        <ResultImage src={srcs[9]} onClick={onClick}></ResultImage>
+        <Button name="Query again"></Button>
       </div>
     </div>
   );
