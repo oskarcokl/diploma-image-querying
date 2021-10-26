a7b940d71f6215ce11333e77612df3c76aea39ee
oskarcokl <oskar.cokl@gmail.com>
2021-10-26 19:05:05 +0200

    feat: Add documentation to DbConnector object.

--- server/db_utils/db_connector.py
+++ server/db_utils/db_connector.py
@@ -1,23 +1,37 @@
 import sys
+import logging
 
 import psycopg2
 
 sys.path.insert(0, ".")
 from .config import config
 
-"""
-DbConncector 
-"""
-
 
 class DbConnector:
+    """
+    DbConncector is used to connect to PostgreSQL database.
+
+    The operations on tables contained in the database are 
+    in the table_operations.py file. The paramaters of the
+    connection are specified in the config.py file in the
+    db_utils module.
+
+    Attributes
+    ----------
+    connection : connection
+        a connection to the PostgreSQL database.
+    cursor : cursor
+        cursor of the connection. Used to exectue operations 
+        on the database.
+    """
+
     def __init__(self):
         try:
             params = config()
             self.connection = psycopg2.connect(**params)
             self.cursor = self.connection.cursor()
         except (Exception, psycopg2.DatabaseError) as e:
-            print(e)
+            logging.error(e)
 
     def close(self):
         try:
@@ -26,4 +40,4 @@ class DbConnector:
             if self.connection is not None:
                 self.connection.close()
         except (Exception) as e:
-            print(e)
+            logging.error(e)
